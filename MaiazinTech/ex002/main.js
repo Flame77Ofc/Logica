a1b32ff1576bbd194405da1be801d2cbb0b07af0
Gabriel Goulart <gabrielgoulartbnu@gmail.com>
2025-03-23 22:02:33 -0300

    Término

---
+++ MaiazinTech/ex002/main.js
@@ -0,0 +1,6 @@
+let num1 = parseInt(prompt("Digite o primeiro número: "))
+let num2 = parseInt(prompt("Digite o segundo número: "))
+
+let soma = num1 + num2
+
+console.log("A soma do número " + num1 + "+" + num2 + " é: " + soma)
