19d749098aea057be942d5f40fa665453e2f5759
Gabriel Goulart <gabrielgoulartbnu@gmail.com>
2025-04-16 19:27:31 -0300

    Exercicio 1

---
+++ Gustavo Caetano/aula001/main.js
@@ -0,0 +1,7 @@
+// Exercício 1
+let numero1 = 2
+let numero2 = 4
+let adicao = numero1 + numero2
+let subtracao = numero1 - numero2
+let multiplicacao = numero1 * numero2
+let divisao = numero1 / numero2
